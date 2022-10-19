efc187009b4554da63e7b5c5e2a7f3244dd1f59f
Sven Wagener <sven@awesome.ug>
2022-10-19 13:08:59 +0200

    Dark theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   title: "Svorm Documentation",
   description: "A JSON based form framework based on svelte and typescript.",
+  theme: 'default-prefers-color-scheme',
   themeConfig: {
+    overrideTheme: 'dark',
     nav: [
       { text: "Home", link: "/" },
       { text: "GitHub", link: "https://github.com/awsmug/svorm" }
